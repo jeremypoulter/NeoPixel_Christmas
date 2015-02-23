0f11bef8261b79e5ee3aa9e13764e970e6e48386
Jeremy Poulter <jeremy@bigjungle.net>
2015-02-23 21:05:54 +0000

    Added switching of the pattwer based on a button press

--- NeoPixel_Christmas.ino
+++ NeoPixel_Christmas.ino
@@ -1,7 +1,9 @@
 #include "TheaterChase.h"
 #include "ColourWipe.h"
 #include "NeoPixelPattern.h"
-#include <MicroTasks\MicroTasks.h>
+
+#include <MicroTasks/MicroTasks.h>
+#include <MicroTasks/Interrupt.h>
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
@@ -25,6 +27,60 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 ColourWipe colourWipe = ColourWipe(&strip);
 TheaterChase theaterChase = TheaterChase(&strip);
 
+NeoPixelPattern *patterns[] = 
+{
+  &colourWipe,
+  &theaterChase
+};
+
+const int numberPatterns = ARRAY_ITEMS(patterns);
+
+Interrupt buttonEvent(0, FALLING);
+
+class SwitchPattern : public Task
+{
+  private:
+    int pattern;
+    EventListener buttonEventListener;
+
+  public:
+    SwitchPattern();
+
+    void setup();
+    unsigned long loop(WakeReason reason);
+};
+
+SwitchPattern::SwitchPattern() :
+  pattern(0), buttonEventListener(this), Task()
+{
+}
+
+void SwitchPattern::setup()
+{
+  buttonEvent.Register(&buttonEventListener);
+  buttonEvent.Attach();
+
+  MicroTasks.startTask(patterns[pattern]);
+}
+
+unsigned long SwitchPattern::loop(WakeReason reason)
+{
+  if (WakeReason_Event == reason &&  buttonEvent.IsTriggered())
+  {
+    MicroTasks.stopTask(patterns[pattern]);
+    if (++pattern >= numberPatterns) {
+      pattern = 0;
+    }
+    MicroTasks.startTask(patterns[pattern]);
+  }
+
+  // return when we next want to be called
+  return MicroTasks.Infinate | MicroTasks.WaitForEvent;
+}
+
+SwitchPattern switchPattern = SwitchPattern();
+
+
 void setup() 
 {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -37,7 +93,7 @@ void setup()
   strip.show(); // Initialize all pixels to 'off'
 
   // Start our task 
-  MicroTasks.startTask(&theaterChase);
+  MicroTasks.startTask(&switchPattern);
 }
 
 void loop() {
